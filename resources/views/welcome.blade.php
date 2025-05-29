1c323fd6fdb3b8636d7503ad8050f18cc5ed4c80
Vitaliy <tsoyvetal@gmail.com>
2025-05-29 13:57:05 +0300

    Render

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,6 @@
         @endif
         --}}
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
