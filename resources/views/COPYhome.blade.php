3d3772363045ca2b4066f6b454bfe9e4ca374daa
Vitaliy <tsoyvetal@gmail.com>
2025-06-10 17:23:38 +0300

    1

---
+++ resources/views/COPYhome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('h1', 'Привет от Хекслета!')
+
+@section('content')
+
+    <p class="">Это простой менеджер задач на Laravel</p>
+
+    <div class="">
+        <a href="https://hexlet.io" class="" target="_blank">
+            Нажми меня</a>
+    </div>
+
+@endsection
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,19 @@
 @extends('layouts.app')
-@section('h1', 'HOME')
+@section('h1', 'Привет от Хекслета!')
+
+@section('content')
+
+    <div class="container mb-4">
+        <p class="lead text-muted mb-4">Это простой менеджер задач на Laravel</p>
+
+        <div class="d-flex gap-3">
+            <a href="https://hexlet.io"
+               class="btn btn-outline-secondary"
+               target="_blank">
+                Нажми меня
+            </a>
+        </div>
+    </div>
+
+@endsection
 
