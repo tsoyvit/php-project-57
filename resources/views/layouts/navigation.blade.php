208963f2ebf2586ec2b7c6e009742bb18cb66464
Vitaliy <tsoyvetal@gmail.com>
2025-06-12 01:00:54 +0300

    @rails/ujs

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,46 +1,58 @@
+<nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900 shadow-md">
+    <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
+        <a href="{{ route('home') }}" class="flex items-center">
+            <span
+                class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{{ __('app.task_manager') }}</span>
+        </a>
 
-    <nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900 shadow-md">
-        <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
-            <a href="{{ route('home') }}" class="flex items-center">
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{{ __('app.task_manager') }}</span>
-            </a>
+        <div class="flex items-center lg:order-2">
 
-            <div class="flex items-center lg:order-2">
+            @auth
+                <a href="{{ route('logout') }}"
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                   class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    {{ __('app.logout') }}
+                </a>
 
-                @auth
-                    {{ html()->form('POST', route('logout'))->open() }}
-                    {{ html()->submit(__('app.logout'))->class('bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded') }}
-                    {{ html()->form()->close() }}
-                @else
-                <a href="{{ route('login') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                    @csrf
+                </form>
+            @else
+                <a href="{{ route('login') }}"
+                   class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     {{ __('app.login') }}
                 </a>
-                <a href="{{ route('register') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
+                <a href="{{ route('register') }}"
+                   class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
                     {{ __('app.registration') }}
                 </a>
-                @endauth
 
-            </div>
+            @endauth
 
-            <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
-                <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                    <li>
-                        <a href="{{ route('tasks.index') }}" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
-                            {{ __('app.tasks') }}
-                        </a>
-                    </li>
-                    <li>
-                        <a href="{{ route('task_statuses.index') }}" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
-                            {{ __('app.statuses') }}
-                        </a>
-                    </li>
-                    <li>
-                        <a href="{{ route('labels.index') }}" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
-                            {{ __('app.tags') }}
-                        </a>
-                    </li>
-                </ul>
-            </div>
         </div>
-    </nav>
+
+        <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
+            <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                <li>
+                    <a href="{{ route('tasks.index') }}"
+                       class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                        {{ __('app.tasks') }}
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('task_statuses.index') }}"
+                       class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                        {{ __('app.statuses') }}
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('labels.index') }}"
+                       class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                        {{ __('app.tags') }}
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
